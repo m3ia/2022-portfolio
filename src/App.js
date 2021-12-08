e02192f7734f86c2a388e41e0805b88e95dda056
m3ia <mnatividad@stackoverflow.com>
2021-12-08 01:56:17 -0800

    add styling

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Skills from "./components/Skills";
 
 export default function App() {
   return (
-    <main className="text-gray-400 bg-gray-900 body-font">
+    <main className="--tw-bg-opacity body-font mb-20 bg-light-grayish-yellow ">
       <Navbar />
       <About />
       <Projects />
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      // background color
+      'light-grayish-yellow': '#eae7dc',
+      // title color
+      'soft-red': '#e98074',
+      // subtitle color/primary text color
+      'dark-grayish-yellow': '#8e8d8a',
+      // secondary text color
+      'grayish-orange': '#d8c3a5',
+    },
     extend: {},
   },
   variants: {
