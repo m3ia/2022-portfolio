3b7bc0911c7eb966c7c444048f3fb6f7e87b10cc
m3ia <mnatividad@stackoverflow.com>
2021-12-08 00:33:18 -0800

    update contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -45,21 +45,20 @@ export default function Contact() {
                 ADDRESS
               </h2>
               <p className="mt-1">
-                97 Warren St. <br />
-                New York, NY 10007
+                Oakland, CA
               </p>
             </div>
             <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
               <h2 className="title-font font-semibold text-white tracking-widest text-xs">
                 EMAIL
               </h2>
-              <a className="text-indigo-400 leading-relaxed">
-                reedbarger@email.com
+              <a className="text-indigo-400 leading-relaxed" href="mailto:meianatividad@gmail.com" target="_blank">
+                Gmail
               </a>
               <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
-                PHONE
+                LINKEDIN
               </h2>
-              <p className="leading-relaxed">123-456-7890</p>
+              <a className="text-indigo-400 leading-relaxed" href="https://www.linkedin.com/in/meiamay" target="_blank">MeiaMay</a>
             </div>
           </div>
         </div>
@@ -68,12 +67,8 @@ export default function Contact() {
           name="contact"
           className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
           <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
-            Hire Me
+            Contact Me
           </h2>
-          <p className="leading-relaxed mb-5">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum
-            suscipit officia aspernatur veritatis. Asperiores, aliquid?
-          </p>
           <div className="relative mb-4">
             <label htmlFor="name" className="leading-7 text-sm text-gray-400">
               Name
